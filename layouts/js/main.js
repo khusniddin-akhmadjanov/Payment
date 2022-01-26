70edef4273c8cbf2b09c1e92e270dd4bfb37b077
khusniddin-akhmadjanov <husniddin1005@inbox.ru>
2022-01-26 10:14:37 +0500

    add card option js file

--- layouts/js/main.js
+++ layouts/js/main.js
@@ -6,17 +6,19 @@ let masterCard = document.getElementById("mastercard-id");
 
 let cardType = document.getElementById("pay-card-title");
 
+let backText = document.getElementById("back-text-id");
+
 visaCard.addEventListener("click", function (e) {
   document.body.style.backgroundColor = "pink";
-  cardType.innerHTML = "Visa";
+  backText.innerHTML = "Visa";
 });
 
 paypalCard.addEventListener("click", function (e) {
   document.body.style.backgroundColor = "lightblue";
-  cardType.innerHTML = "Paypal";
+  backText.innerHTML = "PayPal";
 });
 
 masterCard.addEventListener("click", function (e) {
   document.body.style.backgroundColor = "orange";
-  cardType.innerHTML = "Mastercard";
+  backText.innerHTML = "Mastercard";
 });
