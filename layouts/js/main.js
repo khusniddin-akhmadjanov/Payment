c59ca626b9744c158b3d0e66f594a964e1405d88
khusniddin-akhmadjanov <husniddin1005@inbox.ru>
2022-01-31 12:32:14 +0500

    change index.html file place

--- layouts/js/main.js
+++ layouts/js/main.js
@@ -13,18 +13,20 @@ let backText = document.getElementById("back-text-id");
 visaCard.addEventListener("click", function (e) {
   document.body.style.backgroundColor = "pink";
   backText.innerHTML = "Visa";
-  cardTypeBottom.setAttribute("src", "./images/logos_visaelectron.svg");
-  // src="./images/logos_visaelectron.svg"
+  cardTypeBottom.setAttribute("src", "./layouts/images/logos_visaelectron.svg");
 });
 
 paypalCard.addEventListener("click", function (e) {
   document.body.style.backgroundColor = "lightblue";
   backText.innerHTML = "PayPal";
-  cardTypeBottom.setAttribute("src", "./images/PayPal2007.svg.png");
+  cardTypeBottom.setAttribute("src", "./layouts/images/PayPal2007.svg.png");
 });
 
 masterCard.addEventListener("click", function (e) {
   document.body.style.backgroundColor = "orange";
   backText.innerHTML = "Mastercard";
-  cardTypeBottom.setAttribute("src", "./images/grommet-icons_mastercard.svg");
+  cardTypeBottom.setAttribute(
+    "src",
+    "./layouts/images/grommet-icons_mastercard.svg"
+  );
 });
